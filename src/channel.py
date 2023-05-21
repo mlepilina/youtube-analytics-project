773ad80856016a3f2f073b7852b4a4b0b21d8534
maria <maria.vyukhina@mail.ru>
2023-05-21 12:54:48 +0400

    Homework 2
    - модификация Channel,
    - добавление методов get_service(), to_json()
    - написание тестов

--- src/channel.py
+++ src/channel.py
@@ -15,10 +15,42 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
+        self.subscriber_count = int(channel['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(channel['items'][0]['statistics']['videoCount'])
+        self.view_count = int(channel['items'][0]['statistics']['viewCount'])
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel_id = self.channel_id
+        channel_id = self.__channel_id
         channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        print(channel)
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        '''Возвращает объект для работы с YouTube API'''
+        return youtube
+
+    def to_json(self, file_name):
+        '''Создает файл в формате .json со значениями атрибутов экземпляра класса Channel'''
+        channel_dict = {
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count,
+        }
+
+        with open(file_name, 'wt', encoding='utf-8') as new_json_file:
+            json.dump(channel_dict, new_json_file, ensure_ascii=False)
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,9 +1,12 @@
 """Тесты с использованием pytest для модуля channel."""
+import json
+
 import pytest
-from src.channel import Channel, api_key
+from src.channel import Channel, api_key, youtube
 
 
 def test_api_key():
+    '''Тестируем существование api-ключа'''
     assert api_key == 'AIzaSyDUZLRoOpF7OfeVheZqw9hYMcGZVY6Feok'
 
 
@@ -13,5 +16,25 @@ def moscow_python():
 
 
 def test_print_info(moscow_python):
+    '''Тестируем, что функция ничего не возвращает'''
     assert moscow_python.print_info() is None
 
+
+def test_get_service(moscow_python):
+    '''Тестируем, что функция возвращает объект youtube'''
+    assert moscow_python.get_service() == youtube
+
+
+def test_to_json(moscow_python):
+    '''Тестируем, что функция ничего не возвращает и создается файл .json'''
+    assert moscow_python.to_json('moscowpython.json') is None
+    with open('moscowpython.json') as f:
+        saved_json = json.load(f)
+    assert saved_json['title'] == 'MoscowPython'
+
+
+def test_channel_id(moscow_python):
+    '''Тестируем, что channel_id совпадает с ожидаемым и невозможность установить его напрямую'''
+    assert moscow_python.channel_id == 'UC-OVMPlMA3-YCIeg4z5z23A'
+    with pytest.raises(AttributeError):
+        moscow_python.channel_id = '1234'
